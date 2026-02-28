79a501f43ec969fdb405ac789a1a532830136e8b
ramphy <ramphy_an@outlook.com>
2026-02-28 15:42:15 +0100

    min: CSRF endpoint

--- backend/src/main/java/com/r16a/zeus/auth/AuthController.java
+++ backend/src/main/java/com/r16a/zeus/auth/AuthController.java
@@ -1,9 +1,15 @@
 package com.r16a.zeus.auth;
 
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+    @GetMapping("/csrf-token")
+    public CsrfToken getCsrfToken(CsrfToken csrfToken) {
+        return csrfToken;
+    }
 }
